ee9f2942bad27a13703d8e72374f441209d0ab74
Koji TADOKORO <babylon114514@gmail.com>
2016-05-03 02:27:09 +0900

    Ver.7.1対応

--- src/chara.js
+++ src/chara.js
@@ -464,4 +464,5 @@ Chara.all = [
     new Chara(454, "kisume", "キスメくん", 454),
     new Chara(455, "mjm2", "神殺しマジメくん", 455),
     new Chara(456, "kejime", "ケジメの翼", 456),
+    new Chara(457, "bnkrg3", "力を得たBNKRG姉貴", 457),
 ];
